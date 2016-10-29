071279739d1d20af13860a32a40d51990879cf8d
Miguel Almeida <darkmagusbr@gmail.com>
2016-10-29 20:39:33 -0200

    add cool stuff

--- server/index.js
+++ server/index.js
@@ -1,15 +1,22 @@
+"use strict";
+
 let express = require('express')
 let app = express()
+let cool = require('cool-ascii-faces')
 
 app.set('port', (process.env.PORT || 5000))
 
-app.get('/', function(req, res) {
-    res.send('Hello World Heroku!')
+app.get('/', function(request, response) {
+    response.send('Hello World Heroku!')
 })
 
-app.get('/json', function(req, res) {
-    res.send({message: 'Hello World Heroku!'})
+app.get('/json', function(request, response) {
+    response.send({message: 'Hello World Heroku!'})
 })
+
+app.get('/cool', function(request, response) {
+  response.send(cool());
+});
 
 app.listen(app.get('port'), function(){
     console.log('app is running on port', app.get('port'))
