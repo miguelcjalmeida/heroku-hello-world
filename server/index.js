37a711a58c7e322d2d3f30ba61cdf13826c76539
Miguel Almeida <darkmagusbr@gmail.com>
2016-10-30 00:31:33 -0200

    add postgre example

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@
 let express = require('express')
 let app = express()
 let cool = require('cool-ascii-faces')
+let pg = require('pg');
 
 app.set('port', (process.env.PORT || 5000))
 
@@ -25,6 +26,22 @@ app.get('/times', function(request, response) {
       result += i + ' ' + process.env.DEFAULT_TEXT + ' ';
     response.send(result)
 })
+
+app.get('/db', function (request, response) {
+  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+    client.query('SELECT * FROM test_table', function(err, result) {
+      done();
+
+      if (err) {
+          console.error(err);
+          response.send({error: err});
+          return;
+      }
+
+      response.send(result);
+    });
+  });
+});
 
 app.listen(app.get('port'), function(){
     console.log('app is running on port', app.get('port'))
